3ecc24a16eeb1c2ee08cc0d3983c198da6748bf7
Mirado-Fitahiana <mirado.fitahiana03@gmail.com>
2024-01-27 14:51:48 +0300

    categorie_energie_transmission

--- src/insertion/Categorie.jsx
+++ src/insertion/Categorie.jsx
@@ -9,8 +9,7 @@ import  '../axios_utils';
 import { get, handleChange, post } from '../axios_utils';
 import { Toast } from 'primereact/toast';
 function Categorie() {
-    // const [sideBarOpen,setSideBarOpen] = useState(false);
-    const [customers, setCustomers] = useState([]);
+
     const [data,setData]=useState([]);
     const [loading, setLoading] = useState(true);
     const [formData,setFormData]=useState(new FormData());
@@ -27,13 +26,15 @@ function Categorie() {
     
 
     useEffect(() => {
-        setLoading(true);
+        
         setTimeout(() => {
+            // loading(true)
             setData(get('https://repr-izy-production.up.railway.app/api/v1/Categories')
+            
             .then(response => {
-                // Assuming the data is in response.data.data
+                setLoading(true);
                 setData(response.data.data);
-                console.log(response.data.data); // Access the data here
+                console.log(response.data.data); 
                 setLoading(false);
               })
               .catch(error => {
@@ -41,11 +42,11 @@ function Categorie() {
                 setLoading(false);
               })
             );
-            // setCustomers(data);
+        
             setLoading(false);
             
         }, 1000); 
-        // console.log(data);
+       
     }, []);
     
     
@@ -56,8 +57,7 @@ function Categorie() {
 
     
 
-    // localStorage.setItem('token','eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJVVElMSVNBVEVVUjAwMDUiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3MDYyNTQ0NDYsImV4cCI6MTcwNjM0MDg0Nn0.sXjb3-erDqg7W98MfRiM8XHR19SEtvc2prTbWGQ4daM');
-
+   
     const handleInput=(e)=>{
         handleChange(e,formData,setFormData);
         console.log(formData.values);
@@ -68,7 +68,7 @@ function Categorie() {
         e.preventDefault();
         const response = post(formData,setFormData,'https://repr-izy-production.up.railway.app/api/v1/Categories');
         if (response.error) {
-            setMessage(response.error);
+            setMessage(response.data.error);
             showError();
           
         }else{
@@ -98,15 +98,15 @@ function Categorie() {
                     <Toast ref={toast} />
                 </div>
                 <div className="input-card">
-                    <h4 className="annonce-title" style={{}}>Liste des annonces</h4>
+                    <h4 className="annonce-title" style={{}}>Categories</h4>
                     <DataTable className="custom-datatable" value={data[0]}
                         size="small"
                         paginator rows={10}
                         dataKey="id"
                         loading={loading}
-                        tableStyle={{ minWidth: '60rem', width: '200px', alignItems: 'center', marginLeft: 'auto', marginRight: 'auto' }}
+                        tableStyle={{ minWidth: '15rem', width: '200px', alignItems: 'center', marginLeft: 'auto', marginRight: 'auto' }}
                         globalFilterFields={['nom']}
-                        emptyMessage="Tsisy lty a tsisy.">
+                        emptyMessage="Donnees en attentes">
                         <Column className='column' field="nom" header="Nom" style={{ minWidth: '14rem' }} body={representativeBodyTemplate} filter filterPlaceholder="recherche par style nom" />
                     </DataTable>
                 </div>
--- src/insertion/Transmission.jsx
+++ src/insertion/Transmission.jsx
@@ -1,5 +1,6 @@
 import './form.css';
-import axios from 'axios';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import { Toast } from 'primereact/toast';
 import 'primeflex/primeflex.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
@@ -11,13 +12,27 @@ function Transmission() {
     const toast = useRef(null);
     const [formData,setFormData]=useState(new FormData());
     const [data,setData]=useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         setTimeout(() => {
-            setData(get('https://repr-izy-production.up.railway.app/api/v1/Transmissions'));
+            setData(get('https://repr-izy-production.up.railway.app/api/v1/Transmissions')
+            .then(response => {
+                setData(response.data.data);
+                console.log(response.data.data); 
+                setLoading(false);
+              })
+              .catch(error => {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+              })
+            );
         }, 1000); 
     }, []);
 
+    const representativeBodyTemplate = (rowData) => {
+        return rowData.nom;
+    };
 
     const showSuccess = () => {
         toast.current.show({ severity: 'success', summary: 'Insertion réussie', detail: message, life: 3000 });
@@ -32,9 +47,9 @@ function Transmission() {
     }
     const handleSubmit=(e) =>{
         e.preventDefault();
-        const response= post(formData,setFormData,'https://repr-izy-production.up.railway.app/api/v1/Energies');
+        const response= post(formData,setFormData,'https://repr-izy-production.up.railway.app/api/v1/Transmissions');
         if (response.error) {
-            setMessage(response.error);
+            setMessage(response.data.error);
             showError();
           
         }else{
@@ -63,6 +78,19 @@ function Transmission() {
             </form>
             <Toast ref={toast} />
         </div>
+        <div className="input-card">
+                    <h4 className="annonce-title" style={{}}>Transmission</h4>
+                    <DataTable className="custom-datatable" value={data[0]}
+                        size="small"
+                        paginator rows={10}
+                        dataKey="id"
+                        loading={loading}
+                        tableStyle={{ minWidth: '60rem', width: '200px', alignItems: 'center', marginLeft: 'auto', marginRight: 'auto' }}
+                        globalFilterFields={['nom']}
+                        emptyMessage="Pas de donnees">
+                        <Column className='column' field="nom" header="Nom" style={{ minWidth: '14rem' }} body={representativeBodyTemplate} filter filterPlaceholder="recherche par style nom" />
+                    </DataTable>
+                </div>
         </div>
     </main>
     )
