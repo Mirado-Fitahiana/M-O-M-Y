e3e965da9e1684ebb9267412de272cab82f468f5
Mirado-Fitahiana <mirado.fitahiana03@gmail.com>
2024-01-29 15:35:14 +0300

    Update axios_utils.js

--- src/axios_utils.js
+++ src/axios_utils.js
@@ -44,6 +44,7 @@ export async function get(url) {
                 },
             };
             const response = await axios.request(config);
+            setFormData(new FormData());
             return response;
         } catch (error) {
             console.error('Erreur lors de l\'envoi des données à railway:', error);
@@ -55,6 +56,47 @@ export async function put(url) {
         try {
             let config = {
                 method: 'PUT',
+                maxBodyLength: Infinity,
+                url: url,
+                headers: {
+                    'content-type':'multipart/form-data',
+                    'Authorization':token
+                },
+                
+            };
+            const response = await axios.request(config);
+            return response;
+        } catch (error) {
+            console.error('Erreur lors de l\'envoi des données à railway:', error);
+        }
+}
+
+export async function update(formData,setFormData,url) {
+    const token ='Bearer '+localStorage.getItem('token');
+        try {
+            let config = {
+                method: 'PUT',
+                maxBodyLength: Infinity,
+                url: url,
+                headers: {
+                    'content-type':'multipart/form-data',
+                    'Authorization':token
+                },
+                data:formData
+            };
+            const response = await axios.request(config);
+            setFormData(new FormData())
+            return response;
+        } catch (error) {
+            console.error('Erreur lors de l\'envoi des données à railway:', error);
+        }
+}
+
+export async function Delete(url){
+    const token ='Bearer '+localStorage.getItem('token');
+        try {
+            let config = {
+                method: 'DELETE',
                 maxBodyLength: Infinity,
                 url: url,
                 headers: {
